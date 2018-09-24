1b6cf48f18458bc8fa90f6ac52e56ef63a667346
Gabriel Fernando <gsouza@unigranrio.br>
2018-09-24 05:02:42 -0300

    Atualização do end Point

--- src/main/java/com/example/curso/domain/Categoria.java
+++ src/main/java/com/example/curso/domain/Categoria.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 @Table
@@ -24,6 +26,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
